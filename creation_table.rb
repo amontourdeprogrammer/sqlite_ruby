28df9be414a3919a0411df67c5618c6ae19f56eb
Votre nom ou pseudo <Votre@email.comEliPamp>
2017-05-02 16:55:02 +0200

    ajout de la date

--- creation_table.rb
+++ creation_table.rb
@@ -1,5 +1,14 @@
+require 'sqlite3'
+
+db = SQLite3::Database.new 'file.db'
+
 db.execute <<-SQL
   CREATE TABLE statuts (
-    name VARCHAR(30)
+    name VARCHAR(30),
+    date_statuts DATE
 );
 SQL
+
+
+
+
--- main.rb
+++ main.rb
@@ -4,7 +4,9 @@ db = SQLite3::Database.new 'file.db'
 
 data = ARGV[0]
 
-db.execute("INSERT INTO statuts (name) VALUES (?)", data)
+
+db.execute("INSERT INTO statuts (name, date_statuts) VALUES (?,CURRENT_TIMESTAMP)", data)
+
 
 db.execute 'SELECT * FROM statuts' do |row|
   p row
