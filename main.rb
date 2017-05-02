3cf3d07c8ce1a470f5a5938f0677c5c448abb110
Votre nom ou pseudo <Votre@email.comEliPamp>
2017-05-02 15:45:12 +0200

    création de la table statuts, insertion et selection

--- main.rb
+++ main.rb
@@ -1,1 +1,16 @@
 require 'sqlite3'
+
+db = SQLite3::Database.new 'file.db'
+
+result = db.execute <<-SQL
+  CREATE TABLE statuts (
+    name VARCHAR(30)
+);
+SQL
+
+db.execute('INSERT INTO statuts (name) VALUES ("coucou");')
+
+
+db.execute 'SELECT * FROM statuts' do |row|
+  p row
+end
