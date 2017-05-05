da0a8ee6ff4564b0dd8783e4cab16112377540f8
Cécile Briard <cecile@artips.fr>
2017-05-05 11:24:53 +0200

    Ajout du formulaire et redirection vers la page qui affiche les statuts

--- twitterlike.rb
+++ twitterlike.rb
@@ -4,7 +4,18 @@ require 'sqlite3'
 
 db = SQLite3::Database.new 'file.db'
 
-get '/' do
-  @statuts = db.execute 'SELECT * FROM statuts ' 
+get '/' do 
+    erb :home
+end
+
+post '/statuts' do
+	author = params["author"]
+	content = params["content"]
+	db.execute("INSERT INTO statuts (author, content, date_statuts) VALUES (?, ?,CURRENT_TIMESTAMP)", author, content)
+	redirect to ('/statuts')
+end
+
+get '/statuts' do
+  @statuts = db.execute 'SELECT * FROM statuts'
     erb :index
 end
